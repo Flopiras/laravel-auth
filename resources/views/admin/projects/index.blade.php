a1083bf0c0bc8ca86a0fbabafc5de17ea937a66f
Flopiras <Flopiras@yahoo.it>
2023-08-31 18:25:09 +0200

    fix error

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -12,7 +12,7 @@
                     <div class="card-body">
                       <h5 class="card-title">{{ $project->title }}</h5>
                       <p class="card-text">{{ $project->getAbstract() }}</p>
-                      <a href="{{ route('admin.projects.show')}}" class="btn btn-primary">Show this project</a>
+                      <a href="{{ route('admin.projects.show', $project)}}" class="btn btn-primary">Show this project</a>
                     </div>
                   </div>
             </div>
