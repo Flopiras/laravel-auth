7ac7a500ca20ba927fe861fe5b54eac0543c1205
Flopiras <Flopiras@yahoo.it>
2023-08-31 18:24:02 +0200

    add container

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
         {{-- navbar --}}
         @include('includes.navbar')
 
-        <main class="">
+        <main class="container">
             @yield('content')
         </main>
     </div>
