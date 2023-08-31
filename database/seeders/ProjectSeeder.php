e3bd537d18bcdb1a4a39acf3d13c9bcf656f016d
Flopiras <Flopiras@yahoo.it>
2023-08-31 17:31:33 +0200

    use faker in ProjectSeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,16 +2,28 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Generator $faker): void
     {
-        //
+        for ($i = 1; $i <= 20; $i++) {
+            $project = new Project();
+
+            $project->title = $faker->text(20);
+            $project->slug = Str::slug($project->title, '-');
+            $project->content = $faker->paragraphs(15, true);
+            $project->image = $faker->imageUrl(250, 250);
+
+            $project->save();
+        }
     }
 }
