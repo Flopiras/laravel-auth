c65c387e34481980ddd1d3735e2e940d17e1a216
Flopiras <Flopiras@yahoo.it>
2023-09-01 16:11:26 +0200

    edit GuestHome

--- app/Http/Controllers/Guest/HomeController.php
+++ app/Http/Controllers/Guest/HomeController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('guest.home');
+
+        $projects = Project::orderBy('updated_at', 'DESC')->get();
+
+        return view('guest.home', compact('projects'));
     }
 }
