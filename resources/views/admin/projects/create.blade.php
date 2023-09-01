0ddea5876967ce7d6faff434bc61ef154d4c0d2a
Flopiras <Flopiras@yahoo.it>
2023-09-01 17:11:40 +0200

    add validation

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -17,15 +17,30 @@
             <div class="col-12">
                 <div class="mb-3">
                     <label for="title" class="form-label fw-bold">Titolo</label>
-                    <input type="text" class="form-control" id="title" maxlength="20" placeholder="Inserisci un titolo" value="{{ old('title') }}" required>
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" maxlength="50" placeholder="Inserisci un titolo" value="{{ old('title', '') }}" required>
+
+                    {{-- error message --}}
+                    @error('title')
+                        <div id="titleFeedback" class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
                 </div>
+                
             </div>
             
             {{-- content --}}
             <div class="col-12">
                 <div class="mb-3">
                     <label for="content" class="form-label fw-bold">Testo del progetto</label>
-                    <textarea class="form-control" id="content" rows="10" value="{{ old('content') }}"></textarea>
+                    <textarea class="form-control @error('content') is-invalid @enderror" id="content" rows="10" value="{{ old('content') }}"></textarea>
+
+                    {{-- error message --}}
+                    @error('content')
+                        <div id="contentFeedback" class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
                 </div>
             </div>
 
@@ -33,7 +48,14 @@
             <div class="col-12">
                 <div class="mb-3">
                     <label for="url" class="form-label fw-bold">Link al progetto</label>
-                    <input type="url" class="form-control" id="url" placeholder="Inserisci un link al progetto" value="{{ old('link') }}">
+                    <input type="url" class="form-control @error('link') is-invalid @enderror" id="url" placeholder="Inserisci un link al progetto" value="{{ old('link') }}">
+
+                    {{-- error message --}}
+                    @error('link')
+                        <div id="linkFeedback" class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
                 </div>
             </div>
 
@@ -42,6 +64,13 @@
                 <div class="mb-3">
                     <label for="image" class="form-label fw-bold">Immagine</label>
                     <input type="url" class="form-control @error('image') is-invalid @enderror" id="image" name="image" placeholder="Url immagine" value="{{ old('image') }}">
+
+                    {{-- error message --}}
+                    @error('image')
+                        <div id="imageFeedback" class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
                 </div>
             </div>
             {{-- preview --}}
