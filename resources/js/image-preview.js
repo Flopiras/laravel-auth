635af65980f9c83162d1dc87fd0ce70e4cd0647f
Flopiras <Flopiras@yahoo.it>
2023-09-05 16:58:27 +0200

    change image-preview

--- resources/js/image-preview.js
+++ resources/js/image-preview.js
@@ -2,6 +2,16 @@ const placeholder = "https://marcolanci.it/utils/placeholder.jpg";
 const thumbInput = document.getElementById('image');
 const imagePreview = document.getElementById('image-preview');
 
-thumbInput.addEventListener('input', () => {
-    imagePreview.src = thumbInput.value ? thumbInput.value : placeholder;
+let blobUrl = null;
+
+thumbInput.addEventListener('change', () => {
+    if(thumbInput.files && thumbInput.files[0]) {
+        const file = thumbInput.files[0];
+
+        blobUrl = URL.createObjectURL(file);
+
+        imagePreview.src = blobUrl;
+    } else {
+        imagePreview.src = placeholder;
+    }
 })
